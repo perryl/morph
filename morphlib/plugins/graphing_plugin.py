b9d34766ab19cff05fd95ab2dae04bea6b22a483
Richard Ipsum <richardipsum@fastmail.co.uk>
2014-10-20 19:00:34 +0100

    Update graphing plugin
    At the moment the graphing plugin isn't too useful because it graphs every
    artifact in the entire system, the resulting graph is too large to be useful.
    
    This patch replace the graph-build-depends command with two new commands:
    graph-sources and graph-artifacts. graph-sources constructs a graph of a
    system's sources while graph-artifacts constructs a graph of a stratum's
    artifacts (excluding artifacts that come from other strata i.e. strata
    that the stratum we're graphing depends on)
    
    Both commands omit edges between chunks that come from different strata
    because this is implied by one stratum depending on another, however
    both commands have a --show-all-edges option that can be used to override
    this behaviour.

--- morphlib/plugins/graphing_plugin.py
+++ morphlib/plugins/graphing_plugin.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2012, 2013  Codethink Limited
+# Copyright (C) 2012, 2013, 2014  Codethink Limited
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -20,24 +20,91 @@ import os
 import morphlib
 
 
+class GraphArtifact(object):
+    def __init__(self, source):
+        self.source = source
+
 class GraphingPlugin(cliapp.Plugin):
 
     def enable(self):
-        self.app.add_subcommand('graph-build-depends',
-                                self.graph_build_depends,
-                                arg_synopsis='REPO REF MORPHOLOGY')
+        self.app.add_subcommand('graph-sources',
+                                self.graph_sources,
+                                arg_synopsis='REPO REF SYSTEM')
+
+        self.app.add_subcommand(
+            'graph-artifacts',
+            self.graph_artifacts,
+            arg_synopsis='REPO REF STRATUM')
+
+        group_graphing = 'Graphing Options'
+
+        self.app.settings.boolean(
+            ['show-all-edges'],
+            "A chunk 'foo' in stratum A build depends on "
+            'a stratum B if stratum A depends on B, '
+            "these 'implicit' dependencies are not shown "
+            'by default',
+            group=group_graphing)
 
     def disable(self):
         pass
 
-    def graph_build_depends(self, args):
-        '''Create a visualisation of build dependencies in a system.
+    def add_edge(self, source, dest, f):
+        f.write('  "%s" -> "%s";\n' % (source, dest))
+
+    def add_source_to_graph(self, source, f):
+        shape_name = {
+            'system': 'octagon',
+            'stratum': 'box',
+            'chunk': 'ellipse',
+        }
+
+        f.write('  "%s" [shape=%s];\n' % (source.name,
+                                          shape_name[source.kind]))
+
+    def add_source_artifacts_to_graph(self, source, f):
+        for artifact_name in source.artifacts:
+            self.add_edge(artifact_name, source.name, f)
+
+    def add_source_dependencies_to_graph(self, source, deps, f):
+        for dep in deps:
+            if self.app.settings['show-all-edges']:
+                self.add_edge(source.name, dep.name, f)
+            elif (source.kind in ['stratum', 'system']
+                  or dep.source.kind != 'stratum'):
+                # We don't want to draw edges between chunks
+                # and the strata they depend on, this dependency
+                # is implicit
+                self.add_edge(source.name, dep.name, f)
+
+    def graph_build_prep(self, args):
+        for repo_name, ref, filename in self.app.itertriplets(args):
+            self.app.status(msg='Creating build order for '
+                                '%(repo_name)s %(ref)s %(filename)s',
+                            repo_name=repo_name, ref=ref, filename=filename)
+
+            builder = morphlib.buildcommand.BuildCommand(self.app)
+            srcpool = builder.create_source_pool(repo_name, ref, filename)
+
+            self.app.status(msg='Creating artifact resolver', chatty=True)
+            ar = morphlib.artifactresolver.ArtifactResolver()
+
+            root_artifacts = ar.resolve_root_artifacts(srcpool)
+
+            path, ext = os.path.splitext(filename)
+            _, tail = os.path.split(path)
+            dot_filename = tail + '.gv'
+
+        return srcpool, root_artifacts, dot_filename
+
+    def graph_sources(self, args):
+        '''Create a visualisation of sources in a system.
 
         Command line arguments:
 
         * `REPO` is a repository URL.
         * `REF` is a git reference (usually branch name).
-        * `MORPHOLOGY` is a system morphology.
+        * `SYSTEM` is a system morphology.
 
         This produces a GraphViz DOT file representing all the build
         dependencies within a system, based on information in the
@@ -48,51 +115,139 @@ class GraphingPlugin(cliapp.Plugin):
 
         Example:
 
-            morph graph-build-depends baserock:baserock/morphs master \
-                devel-system-x86_64-generic > foo.dot
-            dot -Tpng foo.dot > foo.png
+            morph graph-sources baserock:baserock/definitions master \
+systems/devel-system-x86_64-generic.morph
+            dot -T png devel-system-x86_64-generic.gv \
+> devel-system-x86_64-generic.gv
 
-        The above would create a picture with the build dependencies of
-        everything in the development system for 64-bit Intel systems.
+        The above creates a picture showing \
+all the sources in the devel system.
 
-        GraphViz is not, currently, part of Baserock, so you need to run
+        Note that edges between chunks that come from different strata are not
+        shown since that dependency is implied by one stratum depending on
+        another. To show all edges regardless you can use the --show-all-edges
+        option.
+
+        GraphViz is not currently part of Baserock so you need to run
         `dot` on another system.
 
         '''
 
-        for repo_name, ref, filename in self.app.itertriplets(args):
-            self.app.status(msg='Creating build order for '
-                                '%(repo_name)s %(ref)s %(filename)s',
-                            repo_name=repo_name, ref=ref, filename=filename)
-            builder = morphlib.buildcommand.BuildCommand(self.app)
-            srcpool = builder.create_source_pool(repo_name, ref, filename)
-            root_artifact = builder.resolve_artifacts(srcpool)
+        _, root_artifacts, dot_filename = self.graph_build_prep(args)
 
-            basename, ext = os.path.splitext(filename)
-            dot_filename = basename + '.gv'
-            dep_fmt = '  "%s" -> "%s";\n'
-            shape_name = {
-                'system': 'octagon',
-                'stratum': 'box',
-                'chunk': 'ellipse',
-            }
+        if len(root_artifacts) > 1:
+            raise cliapp.AppException('Resolved multiple root artifacts')
 
+        root_artifact = root_artifacts[0]
+
+        if root_artifact.source.kind != 'system':
+            cliapp.AppException(
+                'graph-sources expects a system but got a %s: %s'
+                % (root_artifact.kind, root_artifact.name))
+
+        self.app.status(msg='Writing DOT file to %(filename)s',
+                        filename=dot_filename)
+
+        with open(dot_filename, 'w') as f:
+            f.write('digraph "%s" {\n' % dot_filename)
+            sources = set([a.source for a in root_artifact.walk()])
+
+            for source in sources:
+                self.add_source_to_graph(source, f)
+
+                deps = set([GraphArtifact(a.source)
+                            for a in source.dependencies])
+                self.add_source_dependencies_to_graph(source, deps, f)
+
+            f.write('}\n')
+
+    def graph_stratum_root(self, artifact, srcpool, f):
+        if artifact.source.kind != 'stratum':
+            raise cliapp.AppException(
+                'graph-artifacts expects a stratum but got a %s: %s'
+                % (artifact.kind, artifact.name))
+
+        strata = [a for a in artifact.walk() if a.source.kind == 'stratum']
+        strata.remove(artifact)
+
+        foreign_chunks = []
+
+        for stratum in strata:
+            for info in stratum.source.morphology['chunks']:
+                filename = morphlib.util.sanitise_morphology_path(
+                    info.get('morph', info['name']))
+                chunk_source = srcpool.lookup(info['repo'],
+                                              info['ref'], filename)[0]
+                foreign_chunks.append(chunk_source)
+
+            self.add_source_to_graph(stratum.source, f)
+
+        self.add_source_to_graph(artifact.source, f)
+        self.add_source_artifacts_to_graph(artifact.source, f)
+        self.add_source_dependencies_to_graph(
+            artifact.source, artifact.source.dependencies, f)
+
+        sources = set([a.source for a in artifact.walk()
+                       if a.source.kind != 'stratum'])
+
+        assert  all([True for source in sources if source.kind == 'chunk']) \
+                ,'expected only chunk sources'
+
+        for source in sources:
+            if source in foreign_chunks:
+                # skip chunks that aren't in the stratum we're graphing
+                continue
+
+            self.add_source_to_graph(source, f)
+            self.add_source_artifacts_to_graph(source, f)
+            self.add_source_dependencies_to_graph(source,
+                                                  source.dependencies, f)
+
+    def graph_artifacts(self, args):
+        '''Create a visualisation of artifacts in a stratum.
+
+        Command line arguments:
+
+        * `REPO` is a repository URL.
+        * `REF` is a git reference (usually branch name).
+        * `STRATUM` is a system morphology.
+
+        This produces a GraphViz DOT file representing all the build
+        dependencies within a system, based on information in the
+        morphologies.  The GraphViz `dot` program can then be used to
+        create a graphical representation of the dependencies. This
+        can be helpful for inspecting whether there are any problems in
+        the dependencies.
+
+        Example:
+
+            morph graph-artifacts baserock:baserock/definitions master \
+strata/build-essential.morph
+            dot -T png build-essential-minimal.gv > build-essential-minimal.png
+            dot -T png build-essential-devel.gv > build-essential-devel.png
+            dot -T png build-essential-runtime.gv > build-essential-runtime.png
+
+        The above creates a picture for each stratum split in the
+        build-essential stratum. These pictures won't include artifacts
+        that are included as build dependencies from other strata.
+
+        Note that edges between chunks that come from different strata are not
+        shown since that dependency is implied by one stratum depending on
+        another. To show all edges regardless you can use the --show-all-edges
+        option.
+
+        GraphViz is not currently part of Baserock so you need to run
+        `dot` on another system.
+
+        '''
+        srcpool, root_artifacts, _ = self.graph_build_prep(args)
+
+        for artifact in root_artifacts:
+            dot_filename = artifact.name + '.gv'
             self.app.status(msg='Writing DOT file to %(filename)s',
                             filename=dot_filename)
 
             with open(dot_filename, 'w') as f:
-                f.write('digraph "%s" {\n' % basename)
-                for a in root_artifact.walk():
-                    f.write(
-                        '  "%s" [shape=%s];\n' %
-                        (a.name,
-                         shape_name[a.source.morphology['kind']]))
-                    for dep in a.dependencies:
-                        if a.source.morphology['kind'] == 'stratum':
-                            if dep.dependents == [a]:
-                                f.write(dep_fmt %
-                                        (a.name, dep.name))
-                        else:
-                            f.write(dep_fmt % (a.name, dep.name))
+                f.write('digraph "%s" {\n' % dot_filename)
+                self.graph_stratum_root(artifact, srcpool, f)
                 f.write('}\n')
-
