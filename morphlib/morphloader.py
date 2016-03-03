perryl/morph

commit fb6dd955cbc7081f47dc43919075dc4eb3809172

Validate empty configure/build/test/install commands
If a command in a chunk is evaluated to None when a morphology is loaded,
this would previously lead to the subprocess module throwing a 'Coercing to
Unicode' error when morph attempts to run the command during the chunk build.
This change throws a more helpful error if any commands are not valid strings
when the chunk is validated, for example:

ERROR: Field configure-commands[217] expected type <type 'str'>, got <type
'NoneType'> in morphology linux-jetson-tk1

Change-Id: I45220e7deab8c7cd9351507bc998f7ff12797442

morphlib/morphloader.py
@@ -585,10 +585,23 @@ class MorphologyLoader(object):
             cls._validate_products(morphology['name'],
                                    morphology['products'], errors)
 
+        for key in MorphologyDumper.keyorder:
+            if key.endswith('-commands') and key in morphology:
+                cls._validate_commands(morphology['name'], key,
+                                       morphology[key], errors)
+
         if len(errors) == 1:
             raise errors[0]
         elif errors:
             raise MultipleValidationErrors(morphology['name'], errors)
+
+    @classmethod
+    def _validate_commands(cls, morphology_name, key, commands, errors):
+            for cmd_index, cmd in enumerate(commands):
+                if not isinstance(cmd, basestring):
+                    e = InvalidTypeError('%s[%d]' % (key, cmd_index),
+                                         str, type(cmd), morphology_name)
+                    errors.append(e)
 
     @classmethod
     def _validate_products(cls, morphology_name, products, errors):

commit bb139ecf1b9de3b221ff1d93c26190a8623d5d33

Fix some test failures
These seem to have been introduced by commit fb6dd955cbc7081f47dc4391.

Change-Id: I2fb0be5f8829722264f3509f4db9473d19ea2939

morphlib/morphloader.py
@@ -597,11 +597,14 @@ class MorphologyLoader(object):
 
     @classmethod
     def _validate_commands(cls, morphology_name, key, commands, errors):
-            for cmd_index, cmd in enumerate(commands):
-                if not isinstance(cmd, basestring):
-                    e = InvalidTypeError('%s[%d]' % (key, cmd_index),
-                                         str, type(cmd), morphology_name)
-                    errors.append(e)
+        if commands is None:
+            return
+
+        for cmd_index, cmd in enumerate(commands):  # pragma: no cover
+            if not isinstance(cmd, basestring):
+                e = InvalidTypeError('%s[%d]' % (key, cmd_index),
+                                        str, type(cmd), morphology_name)
+                errors.append(e)
 
     @classmethod
     def _validate_products(cls, morphology_name, products, errors):

yarns/implementations.yarn
@@ -860,7 +860,7 @@ Implementations for building systems
     - echo dummy configure
     build-commands:
     - echo The next command will fail
-    - false
+    - 'false'
     EOF
     git add "${chunk_name}".morph
     git commit -m "Update ${chunk_name} build commands"