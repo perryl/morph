perryl/morph

commit d58d8e8f7a4ec03ff14021a4515c8283dad52573

Unify local and remote repo cache modules
There's not really any reason you'd want to use the RemoteRepoCache
class except as a workaround for the slow speed of some LocalRepoCache
operations, so I can't see this ruining anyone's day.

The main reason for doing this is so we can simply the sourceresolver
code. One reason that the sourceresolver class is so hopelessly
complicated is that it right now has to use two incompatible interfaces
for Git repo caches.

I've taken the opportunity to detangle the RepoCache class from the
App class. Now all of the configuration for the RepoCache class is
passed into the constructor explicitly. This makes the class usable from
outside Morph:

    resolver = morphlib.repoaliasresolver.RepoAliasResolver(aliases=[])
    repo_cache = morphlib.repocache.RepoCache('/src/cache/gits', resolver)

Change-Id: I596c81d7645b67504c88e555172a8c238f4f8a66

morphlib/git.py
@@ -58,11 +58,12 @@ class MissingSubmoduleCommitError(cliapp.AppException):
 
 class Submodules(object):
 
-    def __init__(self, app, repo, ref):
-        self.app = app
+    def __init__(self, repo, ref, runcmd_cb=cliapp.runcmd):
         self.repo = repo
         self.ref = ref
         self.submodules = []
+
+        self.runcmd_cb = runcmd_cb
 
     def load(self):
         content = self._read_gitmodules_file()
@@ -76,7 +77,7 @@ class Submodules(object):
     def _read_gitmodules_file(self):
         try:
             # try to read the .gitmodules file from the repo/ref
-            content = gitcmd(self.app.runcmd, 'cat-file', 'blob',
+            content = gitcmd(self.runcmd_cb, 'cat-file', 'blob',
                              '%s:.gitmodules' % self.ref, cwd=self.repo,
                              ignore_fail=True)
 
@@ -100,7 +101,7 @@ class Submodules(object):
                 try:
                     # list objects in the parent repo tree to find the commit
                     # object that corresponds to the submodule
-                    commit = gitcmd(self.app.runcmd, 'ls-tree', self.ref,
+                    commit = gitcmd(self.runcmd_cb, 'ls-tree', self.ref,
                                     submodule.path, cwd=self.repo)
 
                     # read the commit hash from the output

morphlib/__init__.py
@@ -69,15 +69,14 @@ import git
 import gitdir
 import gitindex
 import localartifactcache
-import localrepocache
 import mountableimage
 import morphologyfinder
 import morphology
 import morphloader
 import morphset
 import remoteartifactcache
-import remoterepocache
 import repoaliasresolver
+import repocache
 import savefile
 import source
 import sourcepool

morphlib/buildcommand.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2011-2015  Codethink Limited
+# Copyright (C) 2011-2016  Codethink Limited
 # Copyright © 2015  Richard Ipsum
 #
 # This program is free software; you can redistribute it and/or modify
@@ -47,7 +47,7 @@ class BuildCommand(object):
     def __init__(self, app, build_env = None):
         self.app = app
         self.lac, self.rac = self.new_artifact_caches()
-        self.lrc, self.rrc = self.new_repo_caches()
+        self.repo_cache = morphlib.util.new_repo_cache(self.app)
 
     def build(self, repo_name, ref, filename, original_ref=None):
         '''Build a given system morphology.'''
@@ -76,9 +76,6 @@ class BuildCommand(object):
         '''
         return morphlib.util.new_artifact_caches(self.app.settings)
 
-    def new_repo_caches(self):
-        return morphlib.util.new_repo_caches(self.app)
-
     def new_build_env(self, arch):
         '''Create a new BuildEnvironment instance.'''
         return morphlib.buildenvironment.BuildEnvironment(self.app.settings,
@@ -95,10 +92,8 @@ class BuildCommand(object):
         '''
         self.app.status(msg='Creating source pool', chatty=True)
         srcpool = morphlib.sourceresolver.create_source_pool(
-            self.lrc, self.rrc, repo_name, ref, filenames,
-            cachedir=self.app.settings['cachedir'],
+            self.repo_cache, repo_name, ref, filenames,
             original_ref=original_ref,
-            update_repos=not self.app.settings['no-git-update'],
             status_cb=self.app.status)
         return srcpool
 
@@ -394,8 +389,9 @@ class BuildCommand(object):
         '''Update the local git repository cache with the sources.'''
 
         repo_name = source.repo_name
-        source.repo = self.lrc.get_updated_repo(repo_name, ref=source.sha1)
-        self.lrc.ensure_submodules(source.repo, source.sha1)
+        source.repo = self.repo_cache.get_updated_repo(repo_name,
+                                                       ref=source.sha1)
+        self.repo_cache.ensure_submodules(source.repo, source.sha1)
 
     def cache_artifacts_locally(self, artifacts):
         '''Get artifacts missing from local cache from remote cache.'''
@@ -540,7 +536,7 @@ class BuildCommand(object):
                             '%(sha1)s',
                         name=source.name, sha1=source.sha1[:7])
         builder = morphlib.builder.Builder(
-            self.app, staging_area, self.lac, self.rac, self.lrc,
+            self.app, staging_area, self.lac, self.rac, self.repo_cache,
             self.app.settings['max-jobs'], setup_mounts)
         return builder.build_and_cache(source)
 

morphlib/builder.py
@@ -45,7 +45,7 @@ def extract_sources(app, repo_cache, repo, sha1, srcdir): #pragma: no cover
 
         morphlib.gitdir.checkout_from_cached_repo(repo, sha1, destdir)
         morphlib.git.reset_workdir(app.runcmd, destdir)
-        submodules = morphlib.git.Submodules(app, repo.dirname, sha1)
+        submodules = morphlib.git.Submodules(repo.dirname, sha1, app.runcmd)
         try:
             submodules.load()
         except morphlib.git.NoModulesFileError:
@@ -187,7 +187,7 @@ class BuilderBase(object):
         '''
 
         assert isinstance(self.source.repo,
-                          morphlib.localrepocache.CachedRepo)
+                          morphlib.repocache.CachedRepo)
         meta = {
             'artifact-name': artifact_name,
             'source-name': self.source.name,

morphlib/builder_tests.py
@@ -51,7 +51,7 @@ class FakeSource(object):
         self.name = 'a'
 
         with morphlib.gitdir_tests.allow_nonexistant_git_repos():
-            self.repo = morphlib.localrepocache.CachedRepo(
+            self.repo = morphlib.repocache.CachedRepo(
                 'path', 'repo', 'url')
         self.repo_name = 'url'
         self.original_ref = 'e'

morphlib/definitions_repo.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2015  Codethink Limited
+# Copyright (C) 2015-2016  Codethink Limited
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -127,9 +127,9 @@ class DefinitionsRepo(gitdir.GitDirectory):
         return bbcm()
 
     @contextlib.contextmanager
-    def source_pool(self, lrc, rrc, cachedir, ref, system_filename,
+    def source_pool(self, repo_cache, ref, system_filename,
                     include_local_changes=False, push_local_changes=False,
-                    update_repos=True, status_cb=None, build_ref_prefix=None,
+                    status_cb=None, build_ref_prefix=None,
                     git_user_name=None, git_user_email=None):
         '''Load the system defined in 'morph' and all the sources it contains.
 
@@ -162,15 +162,10 @@ class DefinitionsRepo(gitdir.GitDirectory):
         setting, but that was probably only useful for `morph distbuild` and
         that now uses branch_with_local_changes().
 
-        The 'lrc' and 'rrc' parameters are local and remote Git repo caches.
-        Use morphlib.util.new_repo_caches() to obtain these. The 'cachedir'
-        parameter points to where Git repos are cached by Morph,
-        app.settings['cachedir'] tells you that.
-
-        The 'update_repos' flag allows you to disable updating Git repos, to
-        honour app.settings['no-git-update']. If one of the refs in the build
-        graph is not available locally and update_repos is False, you will see
-        a morphlib.gitdir.InvalidRefError exception.
+        The 'repo_cache' parameter is a morphlib.repocache.RepoCache instance.
+        If update_gits=False is set for this repo cache, and one of the refs in
+        the build graph is not available locally, you will see a
+        morphlib.gitdir.InvalidRefError exception.
 
         The 'status_cb' function will be called if set to output progress and
         status messages to the user.
@@ -200,9 +195,8 @@ class DefinitionsRepo(gitdir.GitDirectory):
                     status_cb(msg='Deciding on task order')
 
                 yield morphlib.sourceresolver.create_source_pool(
-                    lrc, rrc, repo_url, commit, [system_filename],
-                    cachedir=cachedir, original_ref=original_ref,
-                    update_repos=update_repos, status_cb=status_cb)
+                    repo_cache, repo_url, commit, [system_filename],
+                    original_ref=original_ref, status_cb=status_cb)
         else:
             repo_url = self.remote_url
             commit = self.resolve_ref_to_commit(ref)
@@ -212,9 +206,8 @@ class DefinitionsRepo(gitdir.GitDirectory):
 
             try:
                 yield morphlib.sourceresolver.create_source_pool(
-                    lrc, rrc, repo_url, commit, [system_filename],
-                    cachedir=cachedir, original_ref=ref,
-                    update_repos=update_repos, status_cb=status_cb)
+                    repo_cache, repo_url, commit, [system_filename],
+                    original_ref=ref, status_cb=status_cb)
             except morphlib.sourceresolver.InvalidDefinitionsRefError as e:
                 raise cliapp.AppException(
                     'Commit %s wasn\'t found in the "origin" remote %s. '
@@ -332,7 +325,7 @@ class DefinitionsRepoWithApp(DefinitionsRepo):
         self._git_user_name = morphlib.git.get_user_name(app.runcmd)
         self._git_user_email = morphlib.git.get_user_email(app.runcmd)
 
-        self._lrc, self._rrc = morphlib.util.new_repo_caches(app)
+        self.repo_cache = morphlib.util.new_repo_cache(self.app)
 
     def branch_with_local_changes(self, uuid, push=False):
         '''Equivalent to DefinitionsRepo.branch_with_local_changes().'''
@@ -343,22 +336,20 @@ class DefinitionsRepoWithApp(DefinitionsRepo):
             build_ref_prefix=self.app.settings['build-ref-prefix'],
             git_user_name=self._git_user_name,
             git_user_email=self._git_user_email,
-            status_cb=self.app.status,)
+            status_cb=self.app.status)
 
     def source_pool(self, ref, system_filename):
         '''Equivalent to DefinitionsRepo.source_pool().'''
 
         local_changes = self.app.settings['local-changes']
         return DefinitionsRepo.source_pool(
-            self, self._lrc, self._rrc, self.app.settings['cachedir'],
-            ref, system_filename,
+            self, self.repo_cache, ref, system_filename,
             include_local_changes=(local_changes == 'include'),
             push_local_changes=self.app.settings['push-build-branches'],
             build_ref_prefix=self.app.settings['build-ref-prefix'],
             git_user_name=self._git_user_name,
             git_user_email=self._git_user_email,
-            status_cb=self.app.status,
-            update_repos=(not self.app.settings['no-git-update']))
+            status_cb=self.app.status)
 
 def _local_definitions_repo(path, search_for_root, app=None):
     '''Open a local Git repo containing Baserock definitions, at 'path'.

morphlib/localrepocache.py
@@ -1,357 +1,0 @@
-# Copyright (C) 2012-2016  Codethink Limited
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; version 2 of the License.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
-import os
-import urlparse
-import string
-import sys
-import tempfile
-
-import cliapp
-import fs.osfs
-
-import morphlib
-from morphlib.util import word_join_list as _word_join_list
-
-
-# urlparse.urljoin needs to know details of the URL scheme being used.
-# It does not know about git:// by default, so we teach it here.
-gitscheme = ['git']
-urlparse.uses_relative.extend(gitscheme)
-urlparse.uses_netloc.extend(gitscheme)
-urlparse.uses_params.extend(gitscheme)
-urlparse.uses_query.extend(gitscheme)
-urlparse.uses_fragment.extend(gitscheme)
-
-
-def quote_url(url):
-    ''' Convert URIs to strings that only contain digits, letters, % and _.
-
-    NOTE: When changing the code of this function, make sure to also apply
-    the same to the quote_url() function of lorry. Otherwise the git tarballs
-    generated by lorry may no longer be found by morph.
-
-    '''
-    valid_chars = string.digits + string.letters + '%_'
-    transl = lambda x: x if x in valid_chars else '_'
-    return ''.join([transl(x) for x in url])
-
-
-class NoRemote(morphlib.Error):
-
-    def __init__(self, reponame, errors):
-        self.reponame = reponame
-        self.errors = errors
-
-    def __str__(self):
-        return '\n\t'.join(['Cannot find remote git repository: %s' %
-                            self.reponame] + self.errors)
-
-
-class NotCached(morphlib.Error):
-    def __init__(self, reponame):
-        self.reponame = reponame
-
-    def __str__(self):  # pragma: no cover
-        return 'Repository %s is not cached yet' % self.reponame
-
-
-class UpdateError(cliapp.AppException):  # pragma: no cover
-
-    def __init__(self, repo):
-        cliapp.AppException.__init__(
-            self, 'Failed to update cached version of repo %s' % repo)
-
-
-class CachedRepo(morphlib.gitdir.GitDirectory):
-    '''A locally cached Git repository with an origin remote set up.
-
-    On instance of this class represents a locally cached version of a
-    remote Git repository. This remote repository is set up as the
-    'origin' remote.
-
-    Cached repositories are bare mirrors of the upstream.  Locally created
-    branches will be lost the next time the repository updates.
-
-    '''
-    def __init__(self, path, original_name, url):
-        self.original_name = original_name
-        self.url = url
-        self.is_mirror = not url.startswith('file://')
-        self.already_updated = False
-
-        super(CachedRepo, self).__init__(path)
-
-    def __str__(self):  # pragma: no cover
-        return self.url
-
-
-class LocalRepoCache(object):
-
-    '''Manage locally cached git repositories.
-
-    When we build stuff, we need a local copy of the git repository.
-    To avoid having to clone the repositories for every build, we
-    maintain a local cache of the repositories: we first clone the
-    remote repository to the cache, and then make a local clone from
-    the cache to the build environment. This class manages the local
-    cached repositories.
-
-    Repositories may be specified either using a full URL, in a form
-    understood by git(1), or as a repository name to which a base url
-    is prepended. The base urls are given to the class when it is
-    created.
-
-    Instead of cloning via a normal 'git clone' directly from the
-    git server, we first try to download a tarball from a url, and
-    if that works, we unpack the tarball.
-
-    '''
-
-    def __init__(self, app, cachedir, resolver, tarball_base_url=None):
-        self._app = app
-        self.fs = fs.osfs.OSFS('/')
-        self._cachedir = cachedir
-        self._resolver = resolver
-        if tarball_base_url and not tarball_base_url.endswith('/'):
-            tarball_base_url += '/'  # pragma: no cover
-        self._tarball_base_url = tarball_base_url
-        self._cached_repo_objects = {}
-
-    def _git(self, args, **kwargs):  # pragma: no cover
-        '''Execute git command.
-
-        This is a method of its own so that unit tests can easily override
-        all use of the external git command.
-
-        '''
-
-        morphlib.git.gitcmd(self._app.runcmd, *args, **kwargs)
-
-    def _fetch(self, url, path):  # pragma: no cover
-        '''Fetch contents of url into a file.
-
-        This method is meant to be overridden by unit tests.
-
-        '''
-        self._app.status(msg="Trying to fetch %(tarball)s to seed the cache",
-                         tarball=url, chatty=True)
-
-        if self._app.settings['verbose']:
-            verbosity_flags = []
-            kwargs = dict(stderr=sys.stderr)
-        else:
-            verbosity_flags = ['--quiet']
-            kwargs = dict()
-
-        def wget_command():
-            return ['wget'] + verbosity_flags + ['-O-', url]
-
-        self._app.runcmd(wget_command(),
-                         ['tar', '--no-same-owner', '-xf', '-'],
-                         cwd=path, **kwargs)
-
-    def _mkdtemp(self, dirname):  # pragma: no cover
-        '''Creates a temporary directory.
-
-        This method is meant to be overridden by unit tests.
-
-        '''
-        return tempfile.mkdtemp(dir=dirname)
-
-    def _escape(self, url):
-        '''Escape a URL so it can be used as a basename in a file.'''
-
-        # FIXME: The following is a nicer way than to do this.
-        # However, for compatibility, we need to use the same as the
-        # tarball server (set up by Lorry) uses.
-        # return urllib.quote(url, safe='')
-
-        return quote_url(url)
-
-    def _cache_name(self, url):
-        scheme, netloc, path, query, fragment = urlparse.urlsplit(url)
-        if scheme != 'file':
-            path = os.path.join(self._cachedir, self._escape(url))
-        return path
-
-    def has_repo(self, reponame):
-        '''Have we already got a cache of a given repo?'''
-        url = self._resolver.pull_url(reponame)
-        path = self._cache_name(url)
-        return self.fs.exists(path)
-
-    def _clone_with_tarball(self, repourl, path):
-        tarball_url = urlparse.urljoin(self._tarball_base_url,
-                                       self._escape(repourl)) + '.tar'
-        try:
-            self.fs.makedir(path)
-            self._fetch(tarball_url, path)
-            self._git(['config', 'remote.origin.url', repourl], cwd=path)
-            self._git(['config', 'remote.origin.mirror', 'true'], cwd=path)
-            self._git(['config', 'remote.origin.fetch', '+refs/*:refs/*'],
-                      cwd=path)
-        except BaseException as e:  # pragma: no cover
-            if self.fs.exists(path):
-                self.fs.removedir(path, force=True)
-            return False, 'Unable to extract tarball %s: %s' % (
-                tarball_url, e)
-
-        return True, None
-
-    def _cache_repo(self, reponame):
-        '''Clone the given repo into the cache.
-
-        If the repo is already cloned, do nothing.
-
-        '''
-        errors = []
-        if not self.fs.exists(self._cachedir):
-            self.fs.makedir(self._cachedir, recursive=True)
-
-        try:
-            return self._get_repo(reponame)
-        except NotCached as e:
-            pass
-
-        repourl = self._resolver.pull_url(reponame)
-        path = self._cache_name(repourl)
-        if self._tarball_base_url:
-            ok, error = self._clone_with_tarball(repourl, path)
-            if ok:
-                repo = self._get_repo(reponame)
-                self._update_repo(repo)
-                return repo
-            else:
-                errors.append(error)
-                self._app.status(
-                    msg='Using git clone.')
-
-        target = self._mkdtemp(self._cachedir)
-
-        try:
-            self._git(['clone', '--mirror', '-n', repourl, target],
-                      echo_stderr=self._app.settings['debug'])
-        except cliapp.AppException as e:
-            errors.append('Unable to clone from %s to %s: %s' %
-                          (repourl, target, e))
-            if self.fs.exists(target):
-                self.fs.removedir(target, recursive=True, force=True)
-            raise NoRemote(reponame, errors)
-
-        self.fs.rename(target, path)
-
-        repo = self._get_repo(reponame)
-        repo.already_updated = True
-        return repo
-
-    def _get_repo(self, reponame):
-        '''Return an object representing a cached repository.'''
-
-        if reponame in self._cached_repo_objects:
-            return self._cached_repo_objects[reponame]
-        else:
-            repourl = self._resolver.pull_url(reponame)
-            path = self._cache_name(repourl)
-            if self.fs.exists(path):
-                repo = CachedRepo(path, reponame, repourl)
-                self._cached_repo_objects[reponame] = repo
-                return repo
-        raise NotCached(reponame)
-
-    def _update_repo(self, cachedrepo):  # pragma: no cover
-        try:
-            cachedrepo.update_remotes(
-                echo_stderr=self._app.settings['verbose'])
-            cachedrepo.already_updated = True
-        except cliapp.AppException:
-            raise UpdateError(self)
-
-    def get_updated_repo(self, repo_name,
-                         ref=None, refs=None):  # pragma: no cover
-        '''Return object representing cached repository.
-
-        If all the specified refs in 'ref' or 'refs' point to SHA1s that are
-        already in the repository, or --no-git-update is set, then the
-        repository won't be updated.
-
-        '''
-
-        if self._app.settings['no-git-update']:
-            self._app.status(msg='Not updating existing git repository '
-                                 '%(repo_name)s '
-                                 'because of no-git-update being set',
-                             chatty=True,
-                             repo_name=repo_name)
-            return self._get_repo(repo_name)
-
-        if ref is not None and refs is None:
-            refs = (ref,)
-
-        if self.has_repo(repo_name):
-            repo = self._get_repo(repo_name)
-            if refs:
-                required_refs = set(refs)
-                missing_refs = set()
-                for required_ref in required_refs:
-                    if morphlib.git.is_valid_sha1(required_ref):
-                        try:
-                            repo.resolve_ref_to_commit(required_ref)
-                            continue
-                        except morphlib.gitdir.InvalidRefError:
-                            pass
-                    missing_refs.add(required_ref)
-
-                if not missing_refs:
-                    self._app.status(
-                        msg='Not updating git repository %(repo_name)s '
-                            'because it already contains %(sha1s)s',
-                        chatty=True, repo_name=repo_name,
-                        sha1s=_word_join_list(tuple(required_refs)))
-                    return repo
-
-            self._app.status(msg='Updating %(repo_name)s',
-                             repo_name=repo_name)
-            self._update_repo(repo)
-            return repo
-        else:
-            self._app.status(msg='Cloning %(repo_name)s',
-                             repo_name=repo_name)
-            return self._cache_repo(repo_name)
-
-    def ensure_submodules(self, toplevel_repo,
-                          toplevel_ref):  # pragma: no cover
-        '''Ensure any submodules of a given repo are cached and up to date.'''
-
-        def submodules_for_repo(repo_path, ref):
-            try:
-                submodules = morphlib.git.Submodules(self._app, repo_path, ref)
-                submodules.load()
-                return [(submod.url, submod.commit) for submod in submodules]
-            except morphlib.git.NoModulesFileError:
-                return []
-
-        done = set()
-        subs_to_process = submodules_for_repo(toplevel_repo.dirname,
-                                              toplevel_ref)
-        while subs_to_process:
-            url, ref = subs_to_process.pop()
-            done.add((url, ref))
-
-            cached_repo = self.get_updated_repo(url, ref=ref)
-
-            for submod in submodules_for_repo(cached_repo.dirname, ref):
-                if submod not in done:
-                    subs_to_process.append(submod)

commit 116cea24016b28854e439462144a83d31e82a720

morphlib/git: Fix update_submodules
It seems to be broken since d58d8e8f7a4ec03ff14021a4515c8283dad52573
and it's making `morph get-repo` fail with:

    /src/definitions # morph get-repo ansible
    2016-04-06 11:36:31 Loading in all morphologies
    2016-04-06 11:36:32 Checking out ref 0a7124541247cc613352054f4bc0c3e116e0b657 of ansible in ansible stratum
    Traceback (most recent call last):
      File "cliapp/app.py", line 190, in _run
        self.process_args(args)
      File "/src/morph/morphlib/app.py", line 299, in process_args
        cliapp.Application.process_args(self, args)
      File "cliapp/app.py", line 539, in process_args
        method(args[1:])
      File "/src/morph/morphlib/plugins/get_repo_plugin.py", line 142, in get_repo
        chunk_dirname = checkout_chunk(morph, chunk)
      File "/src/morph/morphlib/plugins/get_repo_plugin.py", line 110, in checkout_chunk
        ref or chunk_spec['ref'])
      File "/src/morph/morphlib/plugins/get_repo_plugin.py", line 60, in _clone_repo
        gd.update_submodules(self.app)
      File "/src/morph/morphlib/gitdir.py", line 527, in update_submodules
        morphlib.git.update_submodules(app, self.dirname)
      File "/src/morph/morphlib/git.py", line 150, in update_submodules
        submodules.load()
      File "/src/morph/morphlib/git.py", line 69, in load
        content = self._read_gitmodules_file()
      File "/src/morph/morphlib/git.py", line 82, in _read_gitmodules_file
        ignore_fail=True)
      File "/src/morph/morphlib/git.py", line 306, in gitcmd
        return runcmd(cmdline, **kwargs)
    TypeError: 'str' object is not callable

Change-Id: I8d08eb71a2b0a773bc4322ed46222bea0adbbee1

morphlib/git.py
@@ -128,7 +128,7 @@ def update_submodules(app, repo_dir):  # pragma: no cover
         resolver = morphlib.repoaliasresolver.RepoAliasResolver(
             app.settings['repo-alias'])
         gitcmd(app.runcmd, 'submodule', 'init', cwd=repo_dir)
-        submodules = Submodules(app, repo_dir, 'HEAD')
+        submodules = Submodules(repo_dir, 'HEAD')
         submodules.load()
         for submodule in submodules:
             gitcmd(app.runcmd, 'config', 'submodule.%s.url' % submodule.name,